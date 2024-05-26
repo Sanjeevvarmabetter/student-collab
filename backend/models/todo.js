ad12e8c4507e57083111a31ac41e6891b181753a
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-05-26 15:06:15 +0530

    todo backend added

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,9 +1,16 @@
-//Require mongoose
 const mongoose = require('mongoose');
 
-//Create schema contains a single field named 'name.' 
-//The 'name' field is of type String
-const TodoSchema = new mongoose.Schema({ name: String });
+const TodoSchema = new mongoose.Schema({
+    text: {
+        type:String,
+        required:true,
 
-//Export the Mongoose model with the collection name "Todo"
-module.exports = mongoose.model('Todo', TodoSchema);
+    },
+    comppleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+
+module.exports = mongoose.model('Todo',TodoSchema);
